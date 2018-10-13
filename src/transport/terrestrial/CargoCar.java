9bb729084fda7edf046b48f1411fe2b8443f8ab6
Nikita <Niktos.V@tut.by>
2018-10-13 10:51:46 +0300

    add some changes in hirerachy and make constructors of terrestrial transport

--- src/transport/terrestrial/CargoCar.java
+++ src/transport/terrestrial/CargoCar.java
@@ -5,7 +5,6 @@ import transport.Terrestrial;
 public class CargoCar extends Terrestrial {
     int cargoCapacity;
 
-
     public CargoCar(String name, int speed, int costPerKilometr, int fuelCapacity, int countOfWeels) {
         super(name, speed, costPerKilometr, fuelCapacity, countOfWeels);
     }
