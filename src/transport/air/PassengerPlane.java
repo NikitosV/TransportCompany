53e1bff362faac4d5596673451b13a28da584518
Nikita <Niktos.V@tut.by>
2018-10-14 08:20:15 +0300

    some changes

--- src/transport/air/PassengerPlane.java
+++ src/transport/air/PassengerPlane.java
@@ -5,7 +5,8 @@ import transport.Air;
 public class PassengerPlane extends Air {
     private int countPassengers;
 
-    public PassengerPlane(String name, int speed, int costPerKilometr, int fuelCapacity, int countOfEngines, boolean isReactiveEngine) {
+    public PassengerPlane(String name, int speed, int costPerKilometr, int fuelCapacity, int countOfEngines, boolean isReactiveEngine, int countPassengers) {
         super(name, speed, costPerKilometr, fuelCapacity, countOfEngines, isReactiveEngine);
+        this.countPassengers = countPassengers;
     }
 }
