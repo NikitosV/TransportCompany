390934032ee4fc65178b396dfa2551d9b23fd446
Nikita <Niktos.V@tut.by>
2018-10-14 08:20:43 +0300

    make objects of transports and city

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,33 @@
 package main;
 
+import city.City;
+import transport.air.PassengerPlane;
+import transport.air.SimplePlane;
+import transport.sea.Boat;
+import transport.sea.Tanker;
+import transport.terrestrial.Bus;
+import transport.terrestrial.CargoCar;
+import transport.terrestrial.LightCar;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("============ Создаем города ====================");
+        City city1 = new City("Minsk",true,false);
+        City city2 = new City("Vatikan",false,false);
+        City city3 = new City("Singapur",false,true);
+        City city4 = new City("Sydney",false,true);
+        City city5 = new City("Riga",true,false);
+        System.out.println("================================================");
+        System.out.println("============ Создаем транспорт =================");
+        PassengerPlane planeBoing = new PassengerPlane("Boing",700,50,1000,2,false,230);
+        SimplePlane planeKuku = new SimplePlane("Kukuruznik",150,10,400,1,false);
+        Tanker tanker = new Tanker("Tanker Leon", 100,100,3000,4);
+        Boat boat = new Boat("Honda30",70,5,50,1);
+        LightCar car = new LightCar("Mazda",220,10,400,4);
+        CargoCar cargoCar = new CargoCar("DizelTruck",120,30,700,6);
+        Bus bus = new Bus("Bus-100",90,20,800,8);
+        System.out.println("================================================");
+
 
     }
 }
